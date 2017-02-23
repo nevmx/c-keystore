74aebeaa258d21e30616a29b57063a570e0e24b1
Maxim Neverov <maxsnev@gmail.com>
2017-02-23 18:50:12 -0500

    Working for one key-value pair

--- keystore.c
+++ keystore.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
     32 pods.
@@ -15,6 +16,7 @@
 #define STORE_SIZE 9216
 #define KEY_SIZE 32
 #define VALUE_SIZE 256
+#define POD_SIZE 288
 
 char *store_addr = NULL;
 int kv_store_created = 0;
@@ -50,13 +52,42 @@ int kv_store_create(char *name) {
 }
 
 int kv_store_write(char *key, char *value) {
+    if (kv_store_created != 1) {
+        return -1;
+    }
+
     int index = hash_function(key);
+    char *pod_addr = store_addr + (index * POD_SIZE);
+    
+    // Write key
+    memcpy(pod_addr, key, KEY_SIZE);
+    
+    // Write value
+    memcpy(pod_addr + KEY_SIZE, value, VALUE_SIZE);
+    
+    return 0;
+}
+
+char *kv_store_read(char *key) {
+    if (kv_store_created != 1) {
+        return NULL;
+    }
+    
+    int index = hash_function(key);
+    char *pod_addr = store_addr + (index * POD_SIZE);
+    
+    char *value = (void*)calloc(sizeof(char), VALUE_SIZE);
+    memcpy(value, pod_addr + KEY_SIZE, VALUE_SIZE);
+    return value;
 }
 
 int main(int argc, char** argv) {
+    int result1 = kv_store_create("some_store");
+    int result = kv_store_write("key", "value_hehehehe");
     
-    char* str = argv[1];
+    char* value = kv_store_read("key");
     
-    printf("%s: %d\n", str, hash_function(str));
+    printf("Value: %s\n", value);
+    free(value);
 }
 
